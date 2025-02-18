04d83e658ddfcde2562f35f332963924e7d3d83a
protapkrghosh <protapg518@gmail.com>
2025-02-18 12:16:26 +0600

    added problem two

--- js-problem-solving.js
+++ js-problem-solving.js
@@ -1,6 +1,11 @@
-// 01 Problem
+// Problem-01 : Calculate the cash-out Charge
 function cashOut(money) {
-   if (typeof money !== "number" || money < 0 || typeof money === 'object' || isNaN(money)) {
+   if (
+      typeof money !== "number" ||
+      money < 0 ||
+      typeof money === "object" ||
+      isNaN(money)
+   ) {
       return "Invalid";
    }
 
@@ -8,6 +13,43 @@ function cashOut(money) {
    return charge;
 }
 
-const result = cashOut(2000);
-console.log(result);
+// const result = cashOut(2000);
+// console.log(result);
 
+// Problem-02 : Detect email  valid or not
+function validEmail(email) {
+   let firstCharacter = email[0];
+
+   if (typeof email !== "string") {
+      return "Invalid";
+   }
+
+   if (
+      email.indexOf("@") == -1 ||
+      !email.endsWith(".com") ||
+      email.includes(" ")
+   ) {
+      return false;
+   } else if (
+      firstCharacter === "." ||
+      firstCharacter === "_" ||
+      firstCharacter === "+" ||
+      firstCharacter === "*" ||
+      firstCharacter === "/" ||
+      firstCharacter === "!" ||
+      firstCharacter === "-" ||
+      firstCharacter === "#" ||
+      firstCharacter === "(" ||
+      firstCharacter === ")" ||
+      firstCharacter === "[" ||
+      firstCharacter === "]"
+   ) {
+      return false;
+   } 
+   else {
+      return true;
+   }
+}
+
+const checkEmail = validEmail("ferdous@gmail.com");
+console.log(checkEmail);
